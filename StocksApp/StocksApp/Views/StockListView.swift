95ea75b7a062d70773b1ecc278f86fb2a3fb0385
Rodrigo Conte <rodrigo.conte@ckl.io>
2021-07-29 15:49:17 -0400

    feat: Included search term as observable

---
+++ StocksApp/StocksApp/Views/StockListView.swift
@@ -0,0 +1,64 @@
+//
+//  StockListView.swift
+//  StocksApp
+//
+//  Created by Rodrigo Oliveira on 7/27/21.
+//
+
+import SwiftUI
+
+struct StockListView: View {
+    
+    let stocks: [StockViewModel]
+    
+    var body: some View {
+        List(self.stocks, id: \.symbol) { stock in
+            StockCellView(stock: stock)
+        }
+    }
+}
+
+struct StockCellView: View {
+    
+    let stock: StockViewModel
+    
+    var body: some View {
+        HStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(stock.symbol)
+                        .font(.custom("Arial", size: 22))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 0))
+                    Text(stock.description)
+                        .font(.custom("Arial", size: 18))
+                        .foregroundColor(.gray)
+                }
+            }
+            Spacer()
+            VStack {
+                Text(stock.price)
+                    .foregroundColor(.white)
+                    .font(.custom("Arial", size: 22))
+                Button(stock.change) { }
+                .frame(width: 75)
+                .padding(5)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .cornerRadius(6)
+                
+            }
+        }
+        .background(Color.black)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+    }
+    
+}
+
+struct StockListView_Previews: PreviewProvider {
+    static var previews: some View {
+        let stock = Stock(symbol: "GOOG", description: "Google Stocks", price: 1200, change: "+0,23")
+        return StockListView(stocks: [StockViewModel(stock: stock)])
+    }
+}
