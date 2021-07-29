d9f233140ca1da54e67dea6049ee32ec112b4cd2
Rodrigo Conte <rodrigo.conte@ckl.io>
2021-07-29 15:49:17 -0400

    feat: Included search term as observable

--- StocksApp/StocksApp/ContentView.swift
+++ StocksApp/StocksApp/ContentView.swift
@@ -9,37 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var searchTerm: String = ""
+    @ObservedObject private var stockListViewModel = StockListViewModel()
     
     init() {
         UINavigationBar.appearance().backgroundColor = .black
         UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         UITableView.appearance().backgroundColor = .black
         UITableViewCell.appearance().backgroundColor = .black
+        
+        stockListViewModel.load()
     }
     
     var body: some View {
-        NavigationView {
+        
+        let filteredStock = self.stockListViewModel.searchTerm.isEmpty ? self.stockListViewModel.stocks : self.stockListViewModel.stocks.filter {
+            $0.symbol.starts(with: self.stockListViewModel.searchTerm.uppercased())
+        }
+        
+        return NavigationView {
             ZStack(alignment: .leading) {
                 Color.black
                 Text("July 25 2021")
                     .font(.custom("Arial", size: 32))
                     .fontWeight(.bold)
                     .foregroundColor(Color.gray)
-                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
-                    .offset(y: -210)
+                .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0))
+                    .offset(y: -320)
                 
-                SearchView(searchTerm: $searchTerm)
-                    .offset(y: -160)
+                SearchView(searchTerm: self.$stockListViewModel.searchTerm)
+                    .offset(y: -260)
                 
-            }
-                .navigationBarTitle("Stocks")
+                StockListView(stocks: filteredStock)
+                    .offset(y: 100)
+                
+            }.navigationBarTitle("Stocks")
         }.edgesIgnoringSafeArea(Edge.Set(.bottom))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                
+            ContentView()
+                .previewDevice("iPhone 12")
+        }
     }
 }
